526f0068eed41077d4acd5a8fa7965744f063e69
CS.Wu <bobyme@gmail.com>
2019-03-06 22:58:10 +0800

    Initial Commit

---
+++ Leecode_test/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Leecode_test
+//
+//  Created by Ching ShengWu on 2019/3/6.
+//  Copyright © 2019 Ching ShengWu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
